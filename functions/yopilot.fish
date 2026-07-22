471cd5c1286618c913d25fef3c5573f207b47dba
Mike Wallio <michael.wallio@outlook.com>
2026-07-22 11:09:05 -0400

    Revert to regular yolo

--- functions/yopilot.fish
+++ functions/yopilot.fish
@@ -1,12 +1,3 @@
 function yopilot --wraps=copilot --description 'YOLO mode copilot'
-    for argument in $argv
-        for option in -p --prompt -i --interactive
-            if test "$argument" = "$option"; or string match -q -- "$option=*" "$argument"
-                command copilot --yolo $argv
-                return
-            end
-        end
-    end
-
-    command copilot --experimental -i "/allow-all auto" $argv
+    copilot --yolo $argv
 end
